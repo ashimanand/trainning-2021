18e041574f22522938a5e4078ea8d465e7841c5f
ashimanand <ashim87@yahoo.co.in>
2021-06-02 16:24:26 +0530

    new spring project

---
+++ rws/src/main/java/com/anand/rws/RwsApplication.java
@@ -0,0 +1,13 @@
+package com.anand.rws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RwsApplication.class, args);
+	}
+
+}
---
+++ rws/src/test/java/com/anand/rws/RwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anand.rws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
